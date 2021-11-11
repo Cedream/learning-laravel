a673a5c40d0239e520a612298e6f65c117dd855e
Cedric Thonus <me@cedricthonus.be>
2021-11-11 20:27:05 +0100

    impl. react in laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,23 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes React and other helpers. It's a great starting point while
- * building robust, powerful web applications using React + Laravel.
- */
 
-require('./bootstrap');
+// import Books from './components/Books';
+//require('./bootstrap');
+// require('./components/Books');
 
-/**
- * Next, we will create a fresh React component instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-require('./components/Example');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import Books from './components/Books';
+ 
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <NavLink to='/books'>Books</NavLink>
+            </div>
+            <Routes>
+                <Route exact path='/books' component={Books} />
+            </Routes>
+        </Router>
+    );
+}
+ReactDOM.render(<App />, document.getElementById("app"));
